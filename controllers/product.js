69f2c95ed7e8c02d1627d99f6cd46e64bd005536
Rana <zrana1791@gmail.com>
2023-04-29 21:33:30 +0500

    controller prodoct.js pupdated

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,12 @@
+const mongoose = require("mongoose")
+
 const getAllProducts = async(req, res) => {
-    res.status(200).json({ msg: "Hello" });
+    mongoose.find({});
+    res.status(200);
     res
 }
 const getAllProductsTesting = async(req, res) => {
-    res.status(200).json({ msg: "Hello Testing    " })
+    res.status(200).js
 }
 
 module.exports = { getAllProducts, getAllProductsTesting }
